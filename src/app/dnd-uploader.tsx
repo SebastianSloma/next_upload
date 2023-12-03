7b587b0f446cf8e4d8cefc613224d23cbfe583f6
Sebastian Słoma <sebastiansloma@gmail.com>
2023-12-03 17:22:53 +0100

    added dnd

---
+++ src/app/dnd-uploader.tsx
@@ -0,0 +1,21 @@
+import { UploadDropzone } from "@uploadthing/react";
+ 
+import { OurFileRouter } from "./api/uploadthing/core";
+ 
+export const OurUploadDropzone = () => (
+  <UploadDropzone<OurFileRouter>
+    endpoint="withoutMdwr"
+    onClientUploadComplete={(res) => {
+      // Do something with the response
+      console.log("Files: ", res);
+      alert("Upload Completed");
+    }}
+    onUploadError={(error: Error) => {
+      alert(`ERROR! ${error.message}`);
+    }}
+    onUploadBegin={(name) => {
+      // Do something once upload begins
+      console.log("Uploading: ", name);
+    }}
+  />
+);
