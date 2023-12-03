f4aa93617582f7a45f1f091322cca30679abd575
Sebastian Słoma <sebastiansloma@gmail.com>
2023-12-03 17:17:37 +0100

    added new button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+
 export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
@@ -14,6 +15,7 @@ export default function Home() {
 					priority
 				/>
 			</div>
+    
 
 			<div className='flex justify-evenly w-full'>
 				<Link href='/example-uploader'>Upload</Link>
