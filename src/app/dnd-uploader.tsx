2fe199495525acb85ad432bf8396756909bddbf6
Sebastian Słoma <sebastiansloma@gmail.com>
2023-12-06 16:36:36 +0100

    .

--- src/app/dnd-uploader.tsx
+++ src/app/dnd-uploader.tsx
@@ -1,25 +1,22 @@
-"use client";
+'use client';
 
 import { UploadDropzone } from '@/utils/uploadthing';
 
- 
-
- 
 export default function UploadDnd() {
-  return (
-    <main className="flex flex-col justify-between p-2">
-      <UploadDropzone
-        endpoint="imageUploader"
-        onClientUploadComplete={(res: any) => {
-          // Do something with the response
-          console.log("Files: ", res);
-          alert("Upload Completed");
-        }}
-        onUploadError={(error: Error) => {
-          // Do something with the error.
-          alert(`ERROR! ${error.message}`);
-        }}
-      />
-    </main>
-  );
+	return (
+		<main className='flex flex-col justify-between p-2'>
+			<UploadDropzone
+				endpoint='imageUploader'
+				onClientUploadComplete={(res: any) => {
+					// Do something with the response
+					console.log('Files: ', res);
+					alert('Upload Completed');
+				}}
+				onUploadError={(error: Error) => {
+					// Do something with the error.
+					alert(`ERROR! ${error.message}`);
+				}}
+			/>
+		</main>
+	);
 }
