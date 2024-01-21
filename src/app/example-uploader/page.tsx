c6efa13b7856d105512dbced6b794796bb335baf
Sebastian Słoma <sebastiansloma@gmail.com>
2024-01-21 20:22:44 +0100

    .

--- src/app/example-uploader/page.tsx
+++ src/app/example-uploader/page.tsx
@@ -1,22 +1,22 @@
-"use client";
- 
+'use client';
+
 import { UploadButton } from '../../utils/uploadthing';
- 
+
 export default function ButtonUpload() {
-  return (
-    <main className="flex flex-col justify-between p-20">
-      <UploadButton
-        endpoint="imageUploader"
-        onClientUploadComplete={(res) => {
-          // Do something with the response
-          console.log("Files: ", res);
-          alert("Upload Completed");
-        }}
-        onUploadError={(error: Error) => {
-          // Do something with the error.
-          alert(`ERROR! ${error.message}`);
-        }}
-      />
-    </main>
-  );
+	return (
+		<main className='flex flex-col justify-between p-20'>
+			<UploadButton
+				endpoint='imageUploader'
+				onClientUploadComplete={res => {
+					// Do something with the response
+					console.log('Files: ', res);
+					alert('Upload Completed');
+				}}
+				onUploadError={(error: Error) => {
+					// Do something with the error.
+					alert(`ERROR! ${error.message}`);
+				}}
+			/>
+		</main>
+	);
 }
